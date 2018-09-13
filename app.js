666ac21123e22cf09542013c316302634d7f6261
rakinul <rakinul.huq@mail.mcgill.ca>
2018-09-13 16:59:24 -0400

    uploading the base of Get Smart

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const superagent = require('superagent')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
